83e906e59777a1fae43cf5980d064bdb7d190fcb
Gandhy Leandro Cuasapas <leandritoo0803@gmail.com>
2024-09-26 18:14:14 -0500

    feat: fix to run

--- application/src/main/java/com/seek/candidate_service/CandidateServiceApplication.java
+++ application/src/main/java/com/seek/candidate_service/CandidateServiceApplication.java
@@ -1,14 +1,18 @@
 package com.seek.candidate_service;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@Log4j2
 public class CandidateServiceApplication {
 
     public static void main(String[] args) {
+        log.info("Starting application init");
         SpringApplication.run(CandidateServiceApplication.class, args);
+        log.info("Starting application end");
     }
 }
